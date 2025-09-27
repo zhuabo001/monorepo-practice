1f92ecc4d721133ffbfe3ee52708f6a35cf91740
zhuabo001 <zhuhaobo@192.168.1.6>
2025-09-27 22:12:28 +0800

    创建目录架构，新增prettier和tsconfig等配置文件

---
+++ apps/back-end/src/index.ts
@@ -0,0 +1,2 @@
+console.log('hello from back-end');
+process.exit(0);
---
+++ apps/front-end/src/index.ts
@@ -0,0 +1,4 @@
+console.log('hello from front-end');
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('DOMContentLoaded');
+});
